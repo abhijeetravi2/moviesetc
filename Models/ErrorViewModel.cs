4e0e9f40b7e4dc880e571d432aca06e3441867d0
Abhijeet Kumar Ravi <Abhijeet Kumar Ravi@ABHIJEET>
2022-12-14 13:27:24 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace moviesetc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
